facab2595e1ccb9f2137faeb79d4eb8bc1320f6a
pepe302 <atisha.shiva@gmail.com>
2026-04-09 03:18:14 +0000

    add client.c

--- miniFTP/client.c
+++ miniFTP/client.c
@@ -1,5 +1,34 @@
 #include "socketUtils.h"
 
 int main(){
-    int serverFD=makeip
-}
+    int clientFD=makeIP4socket(); // create socket
+    struct sockaddr_in servAddr = assignSocketAddress();
+
+    if(connect(clientFD,(struct sockaddr*)&servAddr, sizeof(servAddr)) == -1){ //connect to server(ip+port)
+        perror("connect failed");
+        return 1;
+    }
+
+    char filename[256];
+    printf("enter filename to send: ");
+    scanf("%s",filename);
+
+    file *fp = fopen(filename,"rb"); //read file in bin mode
+    if(!fp){
+        perror("file open failed");
+        return 1;
+    }
+
+    send(clientFD,filename,sizeof(filename),0); //firs thing sent- > filenmae, server uses this to create file
+
+    char buffer[1024];
+    size_t bytes;
+    while((bytes = fread(buffer,1,sizeof(buffer),fp))>0){ //read file, send to server
+        send(clientFD,buffer,bytes,0);
+    }
+
+    printf("file sent successfully");
+    fclose(fp);
+    close(clientFD);
+    return 0;
+} 
--- miniFTP/server.c
+++ miniFTP/server.c
@@ -1,31 +1,31 @@
 #include "socketUtils.h"
 
 int main(){
-    int serverFD=makeIP4socket();
+    int serverFD=makeIP4socket(); // opens tcp socket - ready to communicate
     struct sockaddr_in servAddr= assignSocketAddress();
 
-    if(bind(serverFD,(struct sockaddr*)&servAddr, sizeof(servAddr)) = -1){
+    if(bind(serverFD,(struct sockaddr*)&servAddr, sizeof(servAddr)) = -1){ //bind socket to ip+port(send all traffic here)
         perror("bind failed");
         return 1;
     }
 
-    if(listen(serverFD,1) == -1){
+    if(listen(serverFD,1) == -1){ // wait for incoming clients
         perror("listen failed");
         return 1;
     }
 
     printf("server lsitening on %s : %d \n",IP,PORT);
 
-    int clientFD= accept(serverFD,NULL,NULL);
+    int clientFD= accept(serverFD,NULL,NULL); // accept connection - block until client connects
     if(clientFD == -1){
         perror("accept failed");
         return 1;
     }
-
+    /*serverFD->waiting socket, clientFD->actual communication socket*/
     char filename[256];
-    recv(clientFD,filename,sizeof(filename),0);
+    recv(clientFD,filename,sizeof(filename),0); //receive filename
     printf("receiving file: %s\n", filename);
-    file *fp = fopen(filename,"wb");
+    file *fp = fopen(filename,"wb"); //prep to write incoming file
     if(!fp){
         perror("file open failed");
         return 1;
@@ -33,7 +33,7 @@ int main(){
 
     char buffer[1024];
     ssize_t bytes;
-    while((bytes=recv(clientFD,buffer,sizeof(buffer),0))>0){
+    while((bytes=recv(clientFD,buffer,sizeof(buffer),0))>0){ // does it, client closes connctn when recv=0
         fwrite(buffer,1,bytes,fp);
     }
     printf("file received successfully!\n");
